3dad23c2b58333485f045e6fe31109f6800c87b6
Katja Pesonen <katjapesonen1@gmail.com>
2022-05-29 22:16:31 +0300

    Lab done

--- app.js
+++ app.js
@@ -22,18 +22,12 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`
 
 // 👇 Start handling routes here
 const index = require('./routes/index')
-const movieRouter = require('./routes/movies')
 
 app.use('/', index)
-
-app.use('/movies', movieRouter)
-
-app.get('/movies/:movieId', (req, res) => {
-    res.send(req.params);
-})
 
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app)
+
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
-/* GET home page */
+
+/* GET home (index) page */
+
 router.get('/', (req, res, next) => res.render('index'));
 
+
+/* GET movies page */
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then (movies => {
+        console.log(movies);
+        res.render('movies', {movies});
+    })
+    .catch(error => {console.log(error)})
+});
+ 
+
+/* GET movie details page (via the id number) */
+
+router.get("/movies/:movieId", (req, res, next) => {
+
+   const { movieId }  = req.params;
+   console.log('movie id:', movieId);
+ 
+     Movie.findById(movieId)
+        .then((movie) => {
+         console.log(movie);
+          res.render('details', { movie });
+        })
+        .catch((err) => {console.log(err)})
+      });
+   
 
 
 module.exports = router;
