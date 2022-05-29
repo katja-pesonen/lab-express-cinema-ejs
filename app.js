a03b4def646517a8b0deb36c93e9fd96657a46f6
Katja Pesonen <katjapesonen1@gmail.com>
2022-05-29 17:12:42 +0300

    Lab in progress...

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.use('/', index)
 
 app.use('/movies', movieRouter)
 
+app.get('/movies/:movieId', (req, res) => {
+    res.send(req.params);
+})
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app)
--- routes/movies.js
+++ routes/movies.js
@@ -2,16 +2,16 @@ const express = require('express');
 const Movie = require('../models/Movie.model');
 const router = express.Router();
 
-/* GET home page */
+ /* GET movies page */
 
 router.get('/', (req, res, next) => {
-    Movie.find()
-    .then (movies => {
-        console.log(movies);
-        res.render('movies', {movies});
-    })
-    .catch(error => {console.log(error)})
-});
-
+     Movie.find()
+     .then (movies => {
+         console.log(movies);
+         res.render('movies', {movies});
+     })
+     .catch(error => {console.log(error)})
+ });
+  
 
 module.exports = router;
